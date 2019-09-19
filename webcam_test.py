1727712d29bf4ddc10a5085bd7c73b23ab10f1df
Gerson_Urban <gersonurb@gmail.com>
2019-09-18 23:41:13 -0300

    add save function

--- webcam_test.py
+++ webcam_test.py
@@ -9,6 +9,7 @@ import numpy as np
 import cv2
 import imutils
 from imutils.video import VideoStream
+import os
 
 cap = cv2.VideoCapture(0)
 # load OpenCV's Haar cascade for face detection from disk
@@ -17,8 +18,11 @@ path = "/home/pi/Documents/opencv/data/haarcascades/"
 #cv2.cascadeClassifier.load("haarcascade_frontalface_default.xml")
 detector = cv2.CascadeClassifier(path+"haarcascade_frontalface_default.xml")
 
+cont = 0
+
 while(True):
     ret, frame = cap.read()
+    orig = frame.copy()
     frame = imutils.resize(frame, width=400)
     
     #Detect faces in the grayscale frame
@@ -28,11 +32,18 @@ while(True):
         )
     # loop over the face detections and draw then on the frame
     for (x,y,w,h) in rects:
-        cv2.rectangle(frame, (x,y),(x+w,y+h), (255,0,0),2)
+        cv2.rectangle(frame, (x,y),(x+w,y+h), (0,255,0),2)
     
     cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xff==ord('q'):
+    key = cv2.waitKey(1) & 0xff
+    if key == ord('q'):
         break
+    if key == ord('k'):
+        #p = os.path.sep.join("image_{}.png".format(str(cont).zfill(5)))
+        p = "image_{}.png".format(str(cont).zfill(5))
+        print(p)
+        cont += 1
+        cv2.imwrite(p, orig)
 
 cap.release()
 cv2.destroyAllWindows()
